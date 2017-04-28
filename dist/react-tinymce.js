c07775447e16c42f9b07c60b5d89c6594bd3cde8
David Obrite <david@ministrycentered.com>
2017-04-28 10:49:41 -0700

    fix(init): make init async by using available promise

--- dist/react-tinymce.js
+++ dist/react-tinymce.js
@@ -64,6 +64,8 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	'use strict';
 	
+	var _slicedToArray = (function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i['return']) _i['return'](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError('Invalid attempt to destructure non-iterable instance'); } }; })();
+	
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
 	
 	var _react = __webpack_require__(2);
@@ -184,16 +186,19 @@ return /******/ (function(modules) { // webpackBootstrap
 	          editor.setContent(content);
 	        });
 	      }
+	    };
+	
+	    tinymce.init(config).then(function (_ref) {
+	      var _ref2 = _slicedToArray(_ref, 1);
+	
+	      var editor = _ref2[0];
+	
 	      if (hasSetupCallback) {
 	        setupCallback(editor);
 	      }
-	    };
-	
-	    tinymce.init(config);
-	
-	    (0, _reactDom.findDOMNode)(this).style.hidden = '';
-	
-	    this._isInit = true;
+	      _this._isInit = true;
+	      (0, _reactDom.findDOMNode)(_this).style.hidden = '';
+	    });
 	  },
 	
 	  _remove: function _remove() {
